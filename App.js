fdf258eb78275e699f301f5d08f7cf8f85c48df0
Manuel Peyon <peyonmanuel@gmail.com>
2021-12-08 19:50:06 -0300

    wireframe

--- App.js
+++ App.js
@@ -1,21 +1,69 @@
+import {
+	Button,
+	FlatList,
+	Image,
+	Pressable,
+	ScrollView,
+	Text,
+	TextInput,
+	Touchable,
+	TouchableOpacity,
+	View,
+} from 'react-native';
+import React, { useEffect, useState } from 'react';
+
+import { Contador } from './components/Contador';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { styles } from './styles';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [logo, setLogo] = useState(null);
+	useEffect(() => {
+		setLogo(require('./images/Logo.png'));
+	}, []);
+	const suggestions = [
+		'Pintor',
+		'Gasista',
+		'Jardinero',
+		'Plomero',
+		'Electricista',
+		'Instalación',
+	];
+	return (
+		<View style={styles.container}>
+			<StatusBar style={styles.statusBar} />
+			<View style={styles.top}>
+				<View style={styles.imgContainer}>
+					<Image style={styles.img} source={logo} />
+				</View>
+				<Text style={styles.link}>Mi perfil</Text>
+			</View>
+			<View style={styles.searchBar}>
+				<TextInput style={styles.searchInput} placeholder='Buscar'></TextInput>
+				<Pressable style={styles.filters} title='Filtros'>
+					<Text>Filtros</Text>
+				</Pressable>
+			</View>
+			<FlatList
+				numColumns={3}
+				contentContainerStyle={styles.suggestionsContainer}
+				data={suggestions}
+				renderItem={(suggestion) => (
+					<View style={styles.suggestion}>
+						<Text>{suggestion.item}</Text>
+					</View>
+				)}
+				keyExtractor={(item) => item.id}></FlatList>
+			<View style={styles.profiles}>
+				<View style={styles.profile}>
+					<Text>Perfil slider</Text>
+				</View>
+			</View>
+			<View style={styles.footer}>
+				<Text>Profile</Text>
+				<View style={styles.footerSeparator} />
+				<Text>Trabajos cortos</Text>
+			</View>
+		</View>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.js
@@ -0,0 +1,104 @@
+import { StyleSheet } from 'react-native';
+
+const colors = {
+	azul: '#0455BF',
+};
+
+export const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		paddingLeft: 10,
+		paddingRight: 10,
+		paddingTop: 40,
+	},
+
+	top: {
+		width: '100%',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		backgroundColor: '#ffff',
+	},
+	imgContainer: {
+		height: 40,
+		width: '50%',
+		overflow: 'hidden',
+	},
+	img: {
+		height: '100%',
+		width: '100%',
+	},
+	link: {
+		color: colors.azul,
+		fontWeight: 'bold',
+		fontSize: 18,
+	},
+	searchBar: {
+		marginTop: 20,
+		borderRadius: 5,
+		backgroundColor: '#F1F1F1',
+		height: 40,
+		width: '100%',
+		padding: 10,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+	},
+	searchInput: {
+		width: '100%',
+		marginRight: 10,
+	},
+	filters: {
+		position: 'absolute',
+		right: 10,
+		height: '100%',
+		fontFamily: 'roboto',
+		color: '#757575',
+	},
+	suggestionsContainer: {
+		marginTop: 20,
+		alignItems: 'center',
+	},
+	suggestion: {
+		borderRadius: 5,
+		borderColor: colors.azul,
+		borderWidth: 2,
+		width: 90,
+		padding: 50,
+		alignItems: 'center',
+		height: 25,
+		margin: 5,
+	},
+	footer: {
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		width: '100%',
+		padding: 20,
+		elevation: 5,
+		shadowColor: '#000',
+		shadowOffset: { width: -1, height: -1 },
+		shadowOpacity: 0.8,
+		shadowRadius: 1,
+		height: 60,
+	},
+	footerSeparator: {
+		width: 2,
+		height: '75%',
+		position: 'absolute',
+		color: colors.azul,
+	},
+	profiles: {
+		marginTop: 30,
+		marginBottom: 30,
+		width: '100%',
+	},
+	profile: {
+		borderColor: colors.azul,
+		borderWidth: 2,
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: 300,
+		margin: 10,
+	},
+});
